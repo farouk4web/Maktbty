f268892d4c41da4ac2e2823bff722c41945b9153
farouk <faroukabdelhamid.eg@gmail.com>
2024-04-23 23:03:01 +0200

    Upload

---
+++ Maktbty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Maktbty.MvcApplication" Language="C#" %>
---
+++ Maktbty/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Maktbty.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Maktbty.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Maktbty.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
